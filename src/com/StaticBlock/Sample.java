3b59eddabfdbcba1411f4208b689dacca8d93343
yamunadasar <yamuna.dasar@gmail.com>
2022-01-26 12:36:27 -0500

    first commit

---
+++ src/com/StaticBlock/Sample.java
@@ -0,0 +1,28 @@
+package com.StaticBlock;
+
+public class Sample {
+
+}
+
+class A{
+	 
+	 public void m1() {
+		 System.out.println("m1 method");
+	 }
+	 
+}
+class B {
+	 
+	 public void m2() {
+		 System.out.println("m2 method");
+	 }
+		
+		
+}
+class C {
+	 public void m3() {
+		 System.out.println("m3 method");
+	 }
+		
+		
+}
